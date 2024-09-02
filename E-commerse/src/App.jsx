8cdcadaf2aab0992047a5f905874144897d20bf5
ShekharBadhekar <shekharbadhekar@gmail.com>
2024-09-02 19:26:47 +0530

    projects

---
+++ E-commerse/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./Hero.jsx";
+import Navigation from "./Navigation.jsx";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
